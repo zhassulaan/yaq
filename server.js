d69f2137d8dd560cb6ade11452a15b8f28c02a9d
ALEKSEI <ponibrashin@yandex.ru>
2022-10-30 03:02:46 +0700

    deploying react

--- server.js
+++ server.js
@@ -7,8 +7,4 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.listen(PORT);
