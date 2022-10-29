f0bfe1902f009c0f99b85ba9f875c7a355937215
ALEKSEI <ponibrashin@yandex.ru>
2022-10-30 03:44:36 +0700

    deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.static(path.resolve(__dirname, "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT);
