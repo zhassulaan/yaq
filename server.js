3f247a922fb7eb935836823c592e61195028a8f4
ALEKSEI <ponibrashin@yandex.ru>
2022-10-30 04:31:09 +0700

    depl

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.use(express.static(__dirname));
+
 app.use(express.static(path.resolve(__dirname, "build")));
 
 app.get("*", (req, res) => {
