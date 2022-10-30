544f0fa19e79583410e332ecca7ae08db06b3d92
ALEKSEI <ponibrashin@yandex.ru>
2022-10-30 19:21:31 +0700

    deploy1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3000;
 
 const app = express();
 app.use(express.static(__dirname));
